b2398f8d5fe76b1e0c12df4f85917bd232c4696b
Adél Deichler <deichleradel@gmail.com>
2018-05-01 14:43:39 +0200

    Arrays +

--- week-3/day-2/Arrays and functions/src/Reverse.java
+++ week-3/day-2/Arrays and functions/src/Reverse.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Reverse {
     public static void main(String[] args) {
         // - Create an array variable named `aj`
@@ -6,11 +8,24 @@ public class Reverse {
 // - Print the elements of the reversed `aj`
         int[] aj = {3, 4, 5, 6, 7};
         reverseArray(aj);
-        for (int i = 0; i < aj.length; i++) {
+        /*for (int i = 0; i < aj.length; i++) {
             System.out.printf("%d ", aj);
         }
-        System.out.println();
+        System.out.println();*/
+        System.out.println(Arrays.toString(aj));
     }
 
-    
+    private static void reverseArray(int[] aj) {
+        for (int i = 0; i < aj.length / 2; i++) {
+            int j = aj.length - 1 - i;
+            swapTwoElements(aj, i, j);
+        }
+    }
+
+    private static void swapTwoElements(int[] aj, int first, int second) {
+        int temp = aj[first];
+        aj[first] = aj[second];
+        aj[second] = temp;
+    }
+
 }
