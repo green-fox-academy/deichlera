a182c79d79a9eac8d112fdce3876d08c67b731df
Adél Deichler <deichleradel@gmail.com>
2018-06-13 21:03:14 +0200

    greeting upgrade

--- week-9/day-3/greetings/src/main/java/greenfoxacademy/greetings/HelloRESTController.java
+++ week-9/day-3/greetings/src/main/java/greenfoxacademy/greetings/HelloRESTController.java
@@ -2,14 +2,17 @@ package greenfoxacademy.greetings;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
-    Greeting greeting = new Greeting(1,"Hello world!!");
 
-    @RequestMapping("/greeting")
-    public Greeting greet(){
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greet(@RequestParam(value = "name", required = false) String name) {
+        Greeting greeting = new Greeting(1, "Hello " + name + "!!");
         greeting.getId();
         greeting.getContent();
         return greeting;
